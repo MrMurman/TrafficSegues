7e2f97fa7a434548621ffc0f4f69b85eb3c08291
MrMurman <storm2150@yandex.ru>
2021-06-27 15:31:32 +0300

    v1 - some segue experimentation

--- TrafficSegues/ViewController.swift
+++ TrafficSegues/ViewController.swift
@@ -9,11 +9,35 @@ import UIKit
 
 class ViewController: UIViewController {
 
+   
+    @IBOutlet var segueSwitch: UISwitch!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        
     }
 
+    override func shouldPerformSegue(withIdentifier identifier: String, sender: Any?) -> Bool {
+        
+        print("ShouldPerformSegue called")
+        if segueSwitch.isOn && (identifier == "Yellow" || identifier == "Green") {return true} else {return false}
+    }
+    
+    @IBAction func yellowButtonTapped(_ sender: UIButton) {
+    //    if segueSwitch.isOn {
+        shouldPerformSegue(withIdentifier: "Yellow", sender: nil)
+            performSegue(withIdentifier: "Yellow", sender: nil)
+       // }
+    }
+    
 
+    @IBAction func greenButtonTapped(_ sender: UIButton) {
+        if segueSwitch.isOn {
+            performSegue(withIdentifier: "Green", sender: nil)
+        }
+    }
+    
 }
 
